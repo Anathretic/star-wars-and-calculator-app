02bae3f7df0140aec7688a85bbc4051cbc805e61
Konrad <konrad.wojtylo.9@gmail.com>
2024-03-05 17:45:08 +0100

    Added styles to star wars section

--- src/app/littleComponents/HomeworldButton.tsx
+++ src/app/littleComponents/HomeworldButton.tsx
@@ -5,7 +5,7 @@ interface Button {
 }
 
 export const HomeworldButton: React.FC<Button> = ({ link }) => {
-	const [homeworldButtonValue, setHomeworldButtonValue] = useState('Show it!');
+	const [homeworldButtonValue, setHomeworldButtonValue] = useState('Homeworld');
 
 	const getPlanet = async (link: string) => {
 		const planetName = await fetch(link)
@@ -20,6 +20,13 @@ export const HomeworldButton: React.FC<Button> = ({ link }) => {
 	};
 
 	return (
-		<input type='button' onClick={() => getPlanet(link)} value={homeworldButtonValue} className='p-4 bg-red-200 cursor-pointer' />
+		<input
+			type='button'
+			onClick={() => getPlanet(link)}
+			value={homeworldButtonValue}
+			className={`px-6 py-2 m-4 hover:bg-orange-800 duration-300 shadow-xl cursor-pointer rounded-xl w-[156px] ${
+				homeworldButtonValue !== 'Homeworld' ? 'pointer-events-none bg-orange-950 shadow-none' : 'bg-orange-900 shadow-xl'
+			}`}
+		/>
 	);
 };
--- src/app/star-wars/page.tsx
+++ src/app/star-wars/page.tsx
@@ -17,87 +17,167 @@ export default function Page() {
 
 	const { data, error, isLoading } = useSWR<Root>(url + buttonValue + `/?page=${pageNumber}`, fetcher);
 
-	if (error) return <div>Failed to load</div>;
-	if (isLoading) return <div>Loading...</div>;
-	if (data)
-		return (
-			<div>
-				<h1 className='p-4 text-2xl'>Star Wars</h1>
-				<div className='p-2'>
-					<button
-						type='button'
-						className='p-2 bg-red-200'
-						onClick={() => {
-							setButtonValue('people');
-							setPageNumber(1);
-						}}>
-						People
-					</button>
-					<button
-						type='button'
-						className='p-2 mx-2 bg-red-200'
-						onClick={() => {
-							setButtonValue('planets');
-							setPageNumber(1);
-						}}>
-						Planets
-					</button>
-				</div>
-				<div className='p-2'>
-					{buttonValue === 'planets' &&
-						data.results.map((planet: Planets) => (
-							<div key={planet.name}>
-								<span>{planet.name}</span>
-								<span>{planet.rotation_period}</span>
-								<span>{planet.orbital_period}</span>
-								<span>{planet.diameter}</span>
-								<span>{planet.climate}</span>
-								<span>{planet.gravity}</span>
-								<span>{planet.terrain}</span>
-								<span>{planet.surface_water}</span>
-								<span>{planet.population}</span>
-							</div>
-						))}
-					{buttonValue === 'people' &&
-						data.results.map((person: People) => (
-							<div key={person.name}>
-								<span>{person.name}</span>
-								<span>{person.height}</span>
-								<span>{person.mass}</span>
-								<span>{person.hair_color}</span>
-								<span>{person.skin_color}</span>
-								<span>{person.eye_color}</span>
-								<span>{person.birth_year}</span>
-								<span>{person.gender}</span>
-								<HomeworldButton link={person.homeworld} />
-							</div>
-						))}
-					<div>
+	return (
+		<div className='flex items-center justify-center flex-col min-h-screen bg-star-wars bg-cover bg-fixed'>
+			<h1 className='p-4 my-12 text-4xl lg:text-6xl bg-orange-950 shadow-3xl rounded-xl'>Star Wars</h1>
+			{error && <div className='min-h-screen'>Failed to load!</div>}
+			{isLoading && <div className='min-h-screen'>Loading...</div>}
+			{data && (
+				<>
+					<div className='p-2 text-lg 2xl:text-xl flex max-rsm:flex-col'>
 						<button
 							type='button'
-							className='p-2 bg-red-200'
+							className={`p-2 mx-2 w-[128px] 2xl:w-[156px] max-rsm:my-2 rounded-lg ${
+								buttonValue === 'people' ? 'pointer-events-none bg-orange-950' : 'bg-orange-900'
+							} hover:bg-orange-800 duration-300 shadow-3xl`}
 							onClick={() => {
-								if (data.previous !== null) {
-									setPageNumber(pageNumber - 1);
-								}
+								setButtonValue('people');
+								setPageNumber(1);
 							}}>
-							Prev
+							People
 						</button>
 						<button
 							type='button'
-							className='p-2 m-2 bg-red-200'
+							className={`p-2 mx-2 w-[128px] 2xl:w-[156px] max-rsm:my-2 rounded-lg ${
+								buttonValue === 'planets' ? 'pointer-events-none bg-orange-950' : 'bg-orange-900'
+							} hover:bg-orange-800 duration-300 shadow-3xl`}
 							onClick={() => {
-								if (data.next !== null) {
-									setPageNumber(pageNumber + 1);
-								}
+								setButtonValue('planets');
+								setPageNumber(1);
 							}}>
-							Next
+							Planets
 						</button>
 					</div>
-				</div>
-				<Link className='p-2 m-2 bg-red-200' href='/'>
-					Home
-				</Link>
-			</div>
-		);
+					<div className='flex justify-center items-center flex-col p-2 w-full lg:text-lg'>
+						{buttonValue === 'planets' &&
+							data.results.map((planet: Planets) => (
+								<div key={planet.name} className='p-4 m-4 text-center w-full max-w-[600px] rounded-xl bg-orange-950'>
+									<p className='p-2 pb-6 text-2xl'>{planet.name}</p>
+									<p className='p-2'>
+										Rotation period: <br />
+										{planet.rotation_period}
+									</p>
+									<hr />
+									<p className='p-2'>
+										Orbital period: <br />
+										{planet.orbital_period}
+									</p>
+									<hr />
+									<p className='p-2'>
+										Diameter: <br />
+										{planet.diameter}
+									</p>
+									<hr />
+									<p className='p-2'>
+										Climate: <br />
+										{planet.climate
+											.split(', ')
+											.map(string => string.charAt(0).toUpperCase() + string.slice(1))
+											.join(', ')}
+									</p>
+									<hr />
+									<p className='p-2'>
+										Gravity: <br />
+										{planet.gravity}
+									</p>
+									<hr />
+									<p className='p-2'>
+										Terrain: <br />
+										{planet.terrain
+											.split(', ')
+											.map(string => string.charAt(0).toUpperCase() + string.slice(1))
+											.join(', ')}
+									</p>
+									<hr />
+									<p className='p-2'>
+										Surface water: <br />
+										{planet.surface_water}
+									</p>
+									<hr />
+									<p className='p-2'>
+										Population: <br />
+										{planet.population}
+									</p>
+								</div>
+							))}
+						{buttonValue === 'people' &&
+							data.results.map((person: People) => (
+								<div key={person.name} className='p-4 m-4 text-center w-full max-w-[600px] rounded-xl bg-orange-950'>
+									<p className='p-2 pb-6 text-2xl'>{person.name}</p>
+									<p className='p-2'>
+										Height: <br />
+										{person.height + ' cm'}
+									</p>
+									<hr />
+									<p className='p-2'>
+										Weight: <br />
+										{person.mass + ' kg'}
+									</p>
+									<hr />
+									<p className='p-2'>
+										Hair color: <br />
+										{person.hair_color
+											.split(', ')
+											.map(string => string.charAt(0).toUpperCase() + string.slice(1))
+											.join(', ')}
+									</p>
+									<hr />
+									<p className='p-2'>
+										Skin color: <br />
+										{person.skin_color
+											.split(', ')
+											.map(string => string.charAt(0).toUpperCase() + string.slice(1))
+											.join(', ')}
+									</p>
+									<hr />
+									<p className='p-2'>
+										Eyes color: <br />
+										{person.eye_color.charAt(0).toUpperCase() + person.eye_color.slice(1)}
+									</p>
+									<hr />
+									<p className='p-2'>
+										Birth year: <br />
+										{person.birth_year}
+									</p>
+									<hr />
+									<p className='p-2'>
+										Gender: <br />
+										{person.gender.charAt(0).toUpperCase() + person.gender.slice(1)}
+									</p>
+									<hr />
+									<HomeworldButton link={person.homeworld} />
+								</div>
+							))}
+						<div className='py-4 text-lg 2xl:text-xl flex max-rsm:flex-col-reverse'>
+							<button
+								type='button'
+								className='p-2 mx-2 w-[128px] 2xl:w-[156px] max-rsm:my-2 rounded-lg bg-orange-900 hover:bg-orange-800 duration-300 shadow-3xl'
+								onClick={() => {
+									if (data.previous !== null) {
+										setPageNumber(pageNumber - 1);
+									}
+								}}>
+								Prev
+							</button>
+							<button
+								type='button'
+								className='p-2 mx-2 w-[128px] 2xl:w-[156px] max-rsm:my-2 rounded-lg bg-orange-900 hover:bg-orange-800 duration-300 shadow-3xl'
+								onClick={() => {
+									if (data.next !== null) {
+										setPageNumber(pageNumber + 1);
+									}
+								}}>
+								Next
+							</button>
+						</div>
+					</div>
+					<Link
+						className='p-2 my-8 text-xl bg-orange-900 hover:bg-orange-800 duration-300 w-[156px] text-center rounded-lg shadow-3xl'
+						href='/'>
+						Home
+					</Link>
+				</>
+			)}
+		</div>
+	);
 }
